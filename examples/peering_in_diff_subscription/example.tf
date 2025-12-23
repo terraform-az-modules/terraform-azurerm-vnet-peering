b2be02f23b1fac31d460f73740fb87344e274235
abhinav <abhinav.shekhawat@clouddrove.com>
2025-12-23 17:58:41 +0530

    fix: Updated RG, VNet in example.tf of diff_subscription

--- examples/peering_in_diff_subscription/example.tf
+++ examples/peering_in_diff_subscription/example.tf
@@ -1,16 +1,16 @@
 provider "azurerm" {
   features {}
-  subscription_id = ""
+  # subscription_id = ""
 }
 
 provider "azurerm" {
   features {}
   alias           = "peer"
-  subscription_id = "" #change this to other subscription if dns hosted in other subscription.
+  # subscription_id = "" #change this to other subscription if dns hosted in other subscription.
 }
 
 locals {
-  name        = "app"
+  name        = "app1"
   environment = "test"
   label_order = ["name", "environment", "location"]
 }
@@ -19,13 +19,28 @@ locals {
 ## Resource Group module call
 ## Resource group in which all resources will be deployed.
 ##-----------------------------------------------------------------------------
-module "resource_group" {
+module "resource_group_1" {
   source      = "terraform-az-modules/resource-group/azurerm"
   version     = "1.0.3"
   name        = local.name
   environment = local.environment
   label_order = local.label_order
   location    = "northeurope"
+}
+
+##-----------------------------------------------------------------------------
+## Resource Group module call - remote
+##-----------------------------------------------------------------------------
+module "resource_group_2" {
+  providers = {
+    azurerm = azurerm.peer
+  }
+  source      = "terraform-az-modules/resource-group/azurerm"
+  version     = "1.0.3"
+  name        = "app2"
+  environment = local.environment
+  label_order = local.label_order
+  location    = "eastus"
 }
 
 ##-----------------------------------------------------------------------------
@@ -37,20 +52,27 @@ module "vnet" {
   name                = local.name
   environment         = local.environment
   label_order         = local.label_order
-  resource_group_name = module.resource_group.resource_group_name
-  location            = module.resource_group.resource_group_location
+  resource_group_name = module.resource_group_1.resource_group_name
+  location            = module.resource_group_1.resource_group_location
   address_spaces      = ["10.0.0.0/16"]
 }
 
-data "azurerm_resource_group" "peer-rg" {
-  provider = azurerm.peer
-  name     = "example-rg"
-}
+##-----------------------------------------------------------------------------
+## Virtual Network module call - remote.
+##-----------------------------------------------------------------------------
 
-data "azurerm_virtual_network" "peer-staging-vnet" {
-  provider            = azurerm.peer
-  name                = "example-vnet"
-  resource_group_name = data.azurerm_resource_group.peer-rg.name
+module "vnet_remote" {
+  providers = {
+    azurerm = azurerm.peer
+  }
+  source              = "terraform-az-modules/vnet/azurerm"
+  version             = "1.0.3"
+  name                = "remote"
+  environment         = local.environment
+  label_order         = local.label_order
+  resource_group_name = module.resource_group_2.resource_group_name
+  location            = module.resource_group_2.resource_group_location
+  address_spaces      = ["20.0.0.0/16"]
 }
 
 ##-----------------------------------------------------------------------------
@@ -59,15 +81,14 @@ data "azurerm_virtual_network" "peer-staging-vnet" {
 module "vnet_peering" {
   source                        = "../.."
   enabled_diff_subs_peering     = true
-  resource_group_1_name         = module.resource_group.resource_group_name
-  diff_subs_resource_group_name = data.azurerm_resource_group.peer-rg.name
-
+  resource_group_1_name         = module.resource_group_1.resource_group_name
+  diff_subs_resource_group_name = module.resource_group_2.resource_group_name
   providers = {
     azurerm      = azurerm
     azurerm.peer = azurerm.peer
   }
   vnet_1_name         = module.vnet.vnet_name
   vnet_1_id           = module.vnet.vnet_id
-  vnet_diff_subs_name = data.azurerm_virtual_network.peer-staging-vnet.name
-  vnet_diff_subs_id   = data.azurerm_virtual_network.peer-staging-vnet.id
+  vnet_diff_subs_name = module.vnet_remote.vnet_name
+  vnet_diff_subs_id   = module.vnet_remote.vnet_id
 }
